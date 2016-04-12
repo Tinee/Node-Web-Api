5cf15ae88e70ef66467257c8150e4cada13fa478
Marcus Carlsson <Marcus Carlsson>
2016-04-13 00:40:37 +0200

    Added some logic in my index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-var http = require('http')
 
-http.createServer(function(req, res){
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
+var express = require('express');
+var app = express();
 
-    res.end('Hello World\n');
-}).listen(3000 ,'127.0.0.1');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-console.log('Server running at http://127.0.0.1:3000/');
+mongoose.connect('mongodb://localhost/cats');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+
+var cats = require('./cat_routes.js')(app);
+
+var server = app.listen(3000,function(){
+   console.log('Server running at http://127.0.0.1:3000/');
+});
